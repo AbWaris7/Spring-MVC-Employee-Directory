e644064c636faef471e718704876299fef65b87d
abdulwariskhan <hellokhan.ch@gmail.com>
2025-04-10 07:37:11 +0200

    implemented update, delete functionality

--- src/main/java/com/khan/code/Spring/MVC/CRUD/controller/EmployeeController.java
+++ src/main/java/com/khan/code/Spring/MVC/CRUD/controller/EmployeeController.java
@@ -5,8 +5,7 @@ import com.khan.code.Spring.MVC.CRUD.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,8 +26,38 @@ public class EmployeeController {
         theModel.addAttribute("employees", theEmployee);
 
 
-        return "list-employees";
+        return "employees/list-employees";
     }
 
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+        Employee theEmployee = new Employee();
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+    }
+
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+
+        Employee theEmployee = employeeService.findById(theId);
+        theModel.addAttribute("employee", theEmployee);
+
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+        employeeService.save(theEmployee);
+        return "redirect:/employees/list";
+    }
+
+    @GetMapping("/delete")
+    public String deleteEmployee(@RequestParam("employeeId") int theId) {
+        employeeService.deleteById(theId);
+        return "redirect:/employees/list";
+    }
 
 }
--- src/main/java/com/khan/code/Spring/MVC/CRUD/dao/EmployeeRepository.java
+++ src/main/java/com/khan/code/Spring/MVC/CRUD/dao/EmployeeRepository.java
@@ -3,8 +3,14 @@ package com.khan.code.Spring.MVC.CRUD.dao;
 import com.khan.code.Spring.MVC.CRUD.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
+
+    // sort by last name
+
+    public List<Employee> findAllByOrderByFirstNameAsc();
 
 }
